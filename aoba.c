7a8524c943caa6d4e5113ffeff77cea837e26816
nhawshow <andrenfranca1@gmail.com>
2025-07-31 18:44:55 -0300

    Agora isso aqui é BANCO DE DADOS, VAZA C

--- aoba.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Testezinho do Github\n");
-    printf("Testando um pouquinho mais\n");
-    printf("Mais um\n");
-}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Testezinho do Github");
-    printf("Testando um pouquinho mais");
-    printf("Mias um");
-}
